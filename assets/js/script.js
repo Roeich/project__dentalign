c9181ab505c981f49da60072cc8fb443b4e573ba
Roeich <roeichsheikh@gmail.com>
2025-02-16 00:13:49 +0600

    working on contact section

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,34 +1,42 @@
 $(document).ready(function() {
     
     /* _________________ start home page _________________ */
-    
     // Control side menu
     $(".menu_toggleBtn,.header_backDrop").click(function() {
         $(".header").toggleClass("active_header");
     });
+
     // Initialize Bootstrap tooltips
     $('[data-bs-toggle="tooltip"]').tooltip();
 
-    // Copy to Clipboard functionality
-    $(document).on('click', '.copy_clipboard', function () {
-        const textToCopy = $(this).attr('data-clipboard');
-
-        const tempInput = $('<input>');
-        $('body').append(tempInput);
-        tempInput.val(textToCopy).select();
-        document.execCommand('copy');
-        tempInput.remove();
-
-        const tooltip = bootstrap.Tooltip.getInstance(this); // Get tooltip instance
-        tooltip.setContent({ '.tooltip-inner': 'Copied!' }); // Update tooltip content
-        tooltip.show();
-
-        setTimeout(() => {
-            tooltip.setContent({ '.tooltip-inner': 'Click to copy' });
-            tooltip.hide();
-        }, 2000);
+    // features slider section
+    $(".ft_slider1").owlCarousel({
+        items: 3, 
+        loop: true,
+        margin: 16,
+        autoplay: true,
+        autoplayTimeout: 3000, 
+        autoplayHoverPause: false,
+        autoplaySpeed: 3000,
+        smartSpeed: 3000, 
+        slideTransition: 'linear', 
+        dots: false,
+        nav: false
     });
-
+    $(".ft_slider2").owlCarousel({
+        items: 3, 
+        loop: true,
+        margin: 16,
+        autoplay: true,
+        autoplayTimeout: 2500, 
+        autoplayHoverPause: false,
+        autoplaySpeed: 2500,
+        smartSpeed: 2500, 
+        slideTransition: 'linear', 
+        dots: false,
+        nav: false
+    });
+    
 
     /* _________________ end home page _________________ */
     
